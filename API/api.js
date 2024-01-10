227f96f60109a11fa54b4f5a39c4da5cedc747e0
Samson Ayodele <samson.idowu@bankly.ng>
2024-01-10 05:59:30 +0100

    updated

--- API/api.js
+++ API/api.js
@@ -3,18 +3,32 @@ let url =
 
 
   let para = document.getElementById('para');
-   let image = document.getElementById('image');
+  //  let image = document.getElementById('image');
+
+  let root = document.getElementById("root")
 
   async function getCommits(){
     let response = await fetch(url);
 
     let commits = await response.json();
 
+    commits.length
+
     console.log('commits', commits);
 
-    para.innerHTML = commits[1].author.login;
+    // para.innerHTML = commits[1].author.login;
 
-    image.setAttribute('src', commits[1].author.avatar_url)
+    // image.setAttribute('src', commits[1].author.avatar_url)
+
+    let content = commits.map((item) => {
+      return `<div class="content"> 
+                  <p id="para">${item.author.login}</p>
+                   <img src=${item.author.avatar_url} alt="pp" class="image">
+                   <div class="link"><a href=${item.author.html_url} target="_blank">GitHub Profile</a></div>
+              </div>`
+    }).join('')
+
+    root.innerHTML = content;
 
   }
 
